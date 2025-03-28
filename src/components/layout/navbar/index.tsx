eb99924f931318e3dbb3aeff2b6ef5638398f523
Leonardo Gurgel <leo@gurgel.io>
2025-03-28 17:51:34 -0300

    add logo to navbar

--- src/components/layout/navbar/index.tsx
+++ src/components/layout/navbar/index.tsx
@@ -1,9 +1,14 @@
+import { Logo } from "@/components/logos/logo.svg";
+import Link from "next/link";
 import { Navlink } from "./navlink";
 
 export function Navbar() {
   return (
     <header className="absolute top-0 flex w-full p-8">
-      <nav className="flex grow items-center justify-end gap-8">
+      <Link href="/" className="transition-colors hover:text-indigo-500">
+        <Logo className="size-14" />
+      </Link>
+      <nav className="flex grow justify-end gap-8">
         <Navlink href="#about">Sobre mim</Navlink>
         <Navlink href="#experience">Projetos</Navlink>
         <Navlink href="#experience">Experiência</Navlink>
