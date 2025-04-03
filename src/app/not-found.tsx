a8c862d30c15095b8baac51508debe4319b84872
Leonardo Gurgel <leo@gurgel.io>
2025-04-03 16:12:22 -0300

    fix not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,17 +1,17 @@
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
 import "./_styles/globals.css";
 
 export default function NotFound() {
   return (
-    <main className="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
+    <main className="grid h-screen place-items-center px-6 py-24 sm:py-32 lg:px-8">
       <div className="text-center">
-        <p className="text-base font-semibold text-emerald-500">404</p>
-        <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-white sm:text-7xl">
-          Page not found
+        <p className="font-semibold text-base text-emerald-500">404</p>
+        <h1 className="mt-4 text-balance font-semibold text-5xl text-white tracking-tight sm:text-7xl">
+          Página não encontrada
         </h1>
-        <p className="mt-6 text-lg font-medium text-pretty text-zinc-500 sm:text-xl/8">
-          Sorry, we couldn’t find the page you’re looking for.
+        <p className="mt-6 text-pretty font-medium text-lg text-stone-500 sm:text-xl/8">
+          Desculpe, não encontramos o que você procura.
         </p>
         <Button asChild size="xl" className="mt-10">
           <Link href="/">Voltar ao início</Link>
